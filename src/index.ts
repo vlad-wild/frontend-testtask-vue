be01c6b6924071734056b2acc001b0a68e72c995
Vlad Wild <ya.vlash1@yandex.ru>
2025-11-13 22:53:57 +0300

    Добавить точку входа и HTML-шаблон
    - src/index.ts  инициализация Vue-приложения
    - public/index.html  основной шаблон страницы

---
+++ src/index.ts
@@ -0,0 +1,11 @@
+/// <reference path="./types/vue-shims.d.ts" />
+
+import { createApp } from "vue";
+import { createPinia } from "pinia";
+import App from "./App.vue";
+
+const pinia = createPinia();
+const app = createApp(App);
+
+app.use(pinia);
+app.mount("#app");
