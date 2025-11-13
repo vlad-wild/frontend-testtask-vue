6ac11d75630108fe746fdaabd4dcda4d9bd801a1
Vlad Wild <ya.vlash1@yandex.ru>
2025-11-13 22:54:17 +0300

    Создать корневой компонент App.vue
    - App.vue  основной контейнер приложения
    - Подключает компоненты: Search, Table, User

---
+++ src/App.vue
@@ -0,0 +1,26 @@
+<script setup lang="ts">
+import Search from "@components/search.vue";
+import Table from "@components/table.vue";
+import { ref } from "vue";
+
+const searchValue = ref("");
+</script>
+
+<template>
+    <Search
+        @click-search="
+            (value) => {
+                searchValue = value;
+            }
+        "
+    />
+    <Table :search-value="searchValue" />
+</template>
+
+<style>
+@import "tailwindcss";
+
+body {
+    margin: 0;
+}
+</style>
